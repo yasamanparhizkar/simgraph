2a4ea378407efaa2d0dd62ba8227481b4946528b
Yasaman P <74624186+yasamanparhizkar@users.noreply.github.com>
2023-12-14 18:59:13 -0500

    cell grouping analysis

--- code/20_clustering/rtpairwise_example.m
+++ code/20_clustering/rtpairwise_example.m
@@ -14,3 +14,21 @@ model = maxent.trainModel(model,reshape(spikes113, ncells, []),'threshold',15, '
 
 % save the optimized model
 save('opt_model.mat', 'model');
+
+% normalized the model
+if strcmpi(model.type, 'rtpairwise') 
+            
+    nframes = model.nframes;
+    for t=1:nframes
+        % create the equivalent pairwise model
+        model_prim = maxent.createModel(ncells, 'pairwise');
+        model_prim.factors(1:ncells) = model.factors(t*ncells-ncells+1:t*ncells);
+        model_prim.factors(ncells+1:end) = model.factors(nframes*ncells+1:end);
+        model_prim.step_scaling(1:ncells) = model.step_scaling(t*ncells-ncells+1:t*ncells);
+        model_prim.step_scaling(ncells+1:end) = model.step_scaling(nframes*ncells+1:end);
+        % normalize
+        model_prim = maxent.wangLandau(model_prim,'binsize',0.1,'depth',15);
+        model.factors(t*ncells-ncells+1:t*ncells) = model_prim.factors(1:ncells);
+        model.factors(nframes*ncells+1:end) = model_prim.factors(ncells+1:end);
+    end
+end
